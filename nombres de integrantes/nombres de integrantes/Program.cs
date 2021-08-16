9f92681122019463b59b40b554caeecd5e959afd
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-16 15:37:58 -0600

    NOMBRES

---
+++ nombres de integrantes/nombres de integrantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nombres_de_integrantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
